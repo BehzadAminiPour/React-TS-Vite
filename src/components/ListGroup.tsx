c0d6d16292b0680524e56dbf317069a4efc92d27
behzad <behzadaminipour87@gmail.com>
2024-04-15 15:18:43 +0330

    using map method

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
+  const cities = ["Barcelona", "Milan", "Munich", "Nants"];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>Cities</h1>
+      <ul className="list-group">
+        {cities.map((city) => (
+          <li key={city} className="list-group-item">
+            {city}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default ListGroup;
