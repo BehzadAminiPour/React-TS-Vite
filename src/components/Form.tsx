b8e9fea800f21ad59e50921c11ebdce62ba97a61
behzad <behzadaminipour87@gmail.com>
2024-04-21 15:54:03 +0330

    Using useRef hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,17 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: "",
+    age: 0,
+  };
   const handleSubmit = (event: FormEvent) => {
-    console.log("Submitted");
     event.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,13 +19,18 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name :
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input id="name" type="text" className="form-control" ref={nameRef} />
       </div>
       <div className="mb-3">
         <label htmlFor="number" className="form-label">
           Age :
         </label>
-        <input id="number" type="number" className="form-control" />
+        <input
+          id="number"
+          type="number"
+          className="form-control"
+          ref={ageRef}
+        />
       </div>
       <button type="submit" className="btn btn-success">
         Submit
