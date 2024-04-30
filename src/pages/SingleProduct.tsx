6f7d0fee2a8ed3376f6c8289a8fc66d2a1c21862
behzad <behzadaminipour87@gmail.com>
2024-04-30 11:15:24 +0330

    Update SingleProduct

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,11 +1,16 @@
 import { Link, useParams } from "react-router-dom";
-
+import products from "../data";
 const SingleProduct = () => {
   const { cartId } = useParams();
-
+  const product = products.find((product) => product.id === cartId);
+  const { name, image } = product;
   return (
     <div>
       <h3>Product's Code : {cartId}</h3>
+      <h2>{name}</h2>
+      <p>
+        <img src={image} alt={name} />
+      </p>
       <Link to="/cart" className="btn btn-primary">
         Back to cart
       </Link>
